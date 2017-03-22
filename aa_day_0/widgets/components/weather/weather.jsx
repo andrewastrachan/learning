f6f0019e111221f7486e9c807bdbbbac9439ad00
Andrew Strachan <andrewastrachan@outlook.com>
2017-03-22 12:22:52 -0700

    rename day 1 root

--- aa_day_0/widgets/components/weather/weather.jsx
+++ aa_day_0/widgets/components/weather/weather.jsx
@@ -6,22 +6,22 @@ export default class Weather extends React.Component {
   }
 
   componentDidMount() {
-    // navigator.geolocation.getCurrentPosition((pos) => {
-    //   const { latitude, longitude} = pos.coords
-    //   const requestString = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=ef710298d129b9d7c78e2c53fc2a65b9`
-    //   const weatherRequest = new XMLHttpRequest();
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude} = pos.coords
+      const requestString = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=ef710298d129b9d7c78e2c53fc2a65b9`
+      const weatherRequest = new XMLHttpRequest();
 
-    //   weatherRequest.onreadystatechange = (e) => {
-    //     if (weatherRequest.readyState == XMLHttpRequest.DONE) {
-    //       if (weatherRequest.status == 200) {
-    //       } else {
-    //       }
-    //     }
-    //   }
+      weatherRequest.onreadystatechange = (e) => {
+        if (weatherRequest.readyState == XMLHttpRequest.DONE) {
+          if (weatherRequest.status == 200) {
+          } else {
+          }
+        }
+      }
 
-    //   weatherRequest.open('GET', requestString, true)
-    //   weatherRequest.send()
-    // })
+      weatherRequest.open('GET', requestString, true)
+      weatherRequest.send()
+    })
   }
 
   render() {
