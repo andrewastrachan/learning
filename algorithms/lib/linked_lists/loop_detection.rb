9c56b67b353325a5b8f1142434fb9d6a30e3429a
Andrew Strachan <andrewastrachan@outlook.com>
2017-03-24 16:44:21 -0700

    rm some comments

--- algorithms/lib/linked_lists/loop_detection.rb
+++ algorithms/lib/linked_lists/loop_detection.rb
@@ -9,24 +9,16 @@ require 'ap'
 # Should return node at C
 
 # O(N) time and O(N) space
-def loop_detection(node)
-  seen = {}
-  while node = node.next
-    return node if seen[node.object_id]
-    seen[node.object_id] = true
-  end
-  nil
-end
+# def loop_detection(node)
+#   seen = {}
+#   while node = node.next
+#     return node if seen[node.object_id]
+#     seen[node.object_id] = true
+#   end
+#   nil
+# end
 
-# Alternative (Better) Answer:
-#
-# - Our slow runner enters the loop after k steps
-# - This means that our fast runner is 2k steps ahead of our slow runner
-# - This also means that we're k steps into the looped portion
-# - This could also be written as LOOP_SIZE - k steps behind the slow runner
-#    - For instance if the loops size is 4 and we've advanced k steps, then
-
-
+# O(N) time ? and O(1) Space
 def loop_detection(node)
   r1, r2 = node, node
 
@@ -46,18 +38,3 @@ def loop_detection(node)
 
   r2
 end
-
-
-# A - B - C
-#  \_____/
-#
-# index 0 1 2
-# r1loc A B C A
-# r2loc A C B A
-
-# A - B - C - D - E
-#         |_______|
-#
-# index 0 1 2 3 4 5
-# r1loc A B C D
-# r2loc A C E D
